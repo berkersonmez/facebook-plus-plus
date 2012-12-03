bbcf45e5664d46c442ff56f2a8af8540e61e2b29
berkersonmez <brkrsnmz@gmail.com>
2012-12-03 18:42:34 +0200

    INIT

---
+++ MainprojectActivity/src/com/test/maindeneme/MainprojectActivity.java
@@ -0,0 +1,13 @@
+package com.test.maindeneme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainprojectActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
